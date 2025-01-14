0338ac9cb83b2358ba8254fdaf550342eee51bb6
fdhlsyn_ <alifadhilaf92@gmail.com>
2025-01-14 23:29:26 +0700

    Menambahkan Int

--- Aplikasi.c
+++ Aplikasi.c
@@ -5,5 +5,12 @@
     char nama[50];
     int jumlah;
  }Barang;
- 
+
+ Barang daftarBarang[MAX_BARANG];
+ int totalBarang = 0;
+ void tambahBarang(){
+    if (totalBarang >= MAX_BARANG);
+        printf("Kapasitas Penyimpanan Penuh!\n");
+        return;
+ }
  
